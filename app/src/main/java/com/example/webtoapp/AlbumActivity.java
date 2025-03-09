2262de49a5e3c596383f07f15799e5edfc3ab5f7
Jobayer Alam <sheikhnayamul123@gmail.com>
2025-03-10 04:54:52 +0600

    done

--- app/src/main/java/com/example/webtoapp/AlbumActivity.java
+++ app/src/main/java/com/example/webtoapp/AlbumActivity.java
@@ -1,20 +1,55 @@
 package com.example.webtoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AlbumActivity extends AppCompatActivity {
 
+    public static String websiteUrl = "";
+    AppCompatButton daraz, shajgoj, bongoBazar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_album);
+
+        daraz = findViewById(R.id.daraz);
+        shajgoj = findViewById(R.id.shajgoj);
+        bongoBazar = findViewById(R.id.bongoBazar);
+
+        daraz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                websiteUrl = "https://www.daraz.com.bd/";
+                Intent next = new Intent(AlbumActivity.this, DisplayActivity.class);
+                startActivity(next);
+            }
+        });
+        shajgoj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                websiteUrl = "https://www.shajgoj.com/";
+                Intent next = new Intent(AlbumActivity.this, DisplayActivity.class);
+                startActivity(next);
+            }
+        });
+        bongoBazar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                websiteUrl = "https://www.bongo.com.bd/";
+                Intent next = new Intent(AlbumActivity.this, DisplayActivity.class);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/webtoapp/DisplayActivity.java
@@ -0,0 +1,35 @@
+package com.example.webtoapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    WebView display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_display);
+
+        display = findViewById(R.id.display);
+        display.setWebViewClient(new WebViewClient());
+        display.getSettings().setJavaScriptEnabled(true);
+        display.loadUrl(AlbumActivity.websiteUrl);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
